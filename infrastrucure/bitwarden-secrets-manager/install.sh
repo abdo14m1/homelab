5496c33b4d671b01c4d2a8c4e0f6ed95252a7b72
abdo14m1 <abdo14m1@gmail.com>
2025-09-04 18:48:29 +0300

    refactor: using flux to provision all resources in the cluster

--- infrastrucure/bitwarden-secrets-manager/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-helm repo add bitwarden https://charts.bitwarden.com/
-helm repo update
-helm install bitwarden-operator bitwarden/secrets-manager-operator -n secrets-manager
-kubectl create secret generic bw-auth-token -n secrets-manager  --from-literal=token="BW_TOKEN"
